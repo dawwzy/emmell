dec1758a6e08373193e57b5ebbec383c5ea98e69
dawwzy <dawese@icloud.com>
2021-02-13 12:16:06 -0600

    Update matmul_tests.py

--- matmul_tests.py
+++ matmul_tests.py
@@ -8,12 +8,12 @@ def test1():
 
 t1 = timeit.timeit("test1()", number = 10, globals=globals())
 
-print(t1)
+print("Test 1 timing: ", t1)
 
 def test2():
     m1 = tensor([[1, 2],[3, 4]])
     m2 = tensor([[5, 6],[7, 8]])
     return matmul(m1, m2)
 
-assert torch.all(torch.eq(test2(), tensor([[19, 22],[43, 50]])))
-print("Passed Test 2")
+assert torch.all(torch.eq(test2(), tensor([[19, 22],[43, 50]]))), "Failed test 2"
+print("Passed test 2")
