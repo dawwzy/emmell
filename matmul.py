d3b81697d5305a952cb40cae1d8523a335b6f99c
dawwzy <dawese@icloud.com>
2021-02-14 13:48:44 -0600

    Einsum matmul, float tensors for tests.
    Use einsum in matmul.py, and change tensors in matmul_tests.py to floats.

--- matmul.py
+++ matmul.py
@@ -5,9 +5,11 @@ def matmul(a, b):
     ar, ac = a.shape
     br, bc = b.shape
     assert ac==br
-    c = torch.zeros(ar, bc)
+    #c = torch.zeros(ar, bc)
 
-    for i in range(ar):
-        # Convince yourself this works
-        c[i,:] = (a[i].unsqueeze(-1) * b).sum(dim=0)
-    return c
+    #for i in range(ar):
+    #    # Convince yourself this works
+    #    c[i,:] = (a[i,:,None] * b).sum(dim=0)
+    #return c
+
+    return torch.einsum('ik,kj->ij', a, b)
--- matmul_tests.py
+++ matmul_tests.py
@@ -11,8 +11,8 @@ t1 = timeit.timeit("test1()", number = 10, globals=globals())
 print("Test 1 timing: ", t1)
 
 def test2():
-    m1 = tensor([[1, 2],[3, 4]])
-    m2 = tensor([[5, 6],[7, 8]])
+    m1 = tensor([[1.0, 2.0],[3.0, 4.0]])
+    m2 = tensor([[5.0, 6.0],[7.0, 8.0]])
     return matmul(m1, m2)
 
 print("Test 2 ... ", end="")
