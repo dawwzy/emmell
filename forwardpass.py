cc447740f4bd6e16a78375d46ebdc522f51c39ba
dawwzy <dawese@icloud.com>
2021-02-21 16:22:42 -0600

    Update forwardpass.py

--- forwardpass.py
+++ forwardpass.py
@@ -1,5 +1,11 @@
 import torch
 from torch import tensor
 
-def forwardpass:
+def forwardpass():
     return
+
+def lin(x, w, b): return x @ w + b
+
+def normalize(x, m, s): return (x-m)/s
+
+def test_near_zero(a, tol=1e-3): assert a.abs()<tol, f"Near zero: {a}"
